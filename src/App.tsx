a3fe85fa3c3ba1d4bd3678f5b57d9b5b37112457
Mayshara Fernandes <mayshara@alunos.utfpr.edu.br>
2020-07-14 19:07:43 -0300

    feat: created standard style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import GlobalStyles from "./styles/GlobalStyles";
 
 function App() {
-  return <h1>Teste</h1>;
+  return (
+    <>
+      <h1> Rocketseat </h1>
+
+      <GlobalStyles />
+    </>
+  );
 }
 
 export default App;
